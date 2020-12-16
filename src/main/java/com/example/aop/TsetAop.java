4d09e8bd6b5598c6b7246dacd10b3adae3c52614
ChenAugustus <752517933@qq.com>
2020-12-16 22:12:45 +0800

    upadte aop

--- src/main/java/com/example/aop/TsetAop.java
+++ src/main/java/com/example/aop/TsetAop.java
@@ -1,8 +1,21 @@
 package com.example.aop;
 
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author cyx
  * @date 2020/12/16
  */
 public class TsetAop {
+
+    @Test
+    public void testAopAnno() {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean3.xml");
+        User user = context.getBean("user", User.class);
+        user.add();
+    }
+
 }
